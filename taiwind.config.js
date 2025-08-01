06283011927dddfe9229852ade282e5c0effe292
hapham <hattp@carmd.com>
2025-08-01 11:29:44 +0700

    feat: add taiwind config

---
+++ taiwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          vintage: ['"EB Garamond"', 'serif'],
+        },
+        colors: {
+          vintageBg: '#faf4e1',
+          vintageBorder: '#d6b97b',
+          vintageAccent: '#7c4f20',
+          vintageSoft: '#ede0d4',
+        },
+      },
+    },
+  };
